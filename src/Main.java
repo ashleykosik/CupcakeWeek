10bfc5d2d77f1ee5495dc3c24cc21551fb9a91c5
Ashley Kosik <ashley.kosik@gmail.com>
2023-06-05 12:32:07 -0400

    created cupcakes & cupcakeMenu

---
+++ src/Main.java
@@ -0,0 +1,82 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        class Cupcake {
+
+            public double price;
+
+            public double getPrice() {
+                return price;
+            }
+
+            public void setPrice(double price) {
+                this.price = price;
+            }
+
+            public void type() {
+                System.out.println("A basic, generic cupcake, with vanilla frosting");
+            }
+        }
+            class RedVelvet extends Cupcake
+            {
+                public void type()
+                {
+                    System.out.println("A red velvet based cupcake, with cream cheese frosting");
+                }
+            }
+
+            class Chocolate extends Cupcake
+            {
+                public void type()
+                {
+                    System.out.println("A chocolate based cupcake, with chocolate frosting");
+                }
+            }
+
+
+
+        ArrayList<Cupcake> cupcakeMenu = new ArrayList<Cupcake>();
+        Cupcake cupcake = new Cupcake();
+        RedVelvet redVelvet = new RedVelvet();
+        Chocolate chocolate = new Chocolate();
+
+        System.out.println("We are in the middle of creating the cupcake menu. We currently have three cupcakes on the menu but we need to decide on pricing.");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("We are deciding on the price for our standard cupcake. Here is the description:");
+        cupcake.type();
+        System.out.println("How much would you like to charge for the cupcake?");
+        // collecting price as a string to convert later
+        String priceText = input.nextLine();
+        Double price = Double.parseDouble(priceText);
+        cupcake.setPrice(price);
+
+        System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description: ");
+        redVelvet.type();
+        System.out.println("How much would you like to charge for the cupcake?");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        redVelvet.setPrice(price);
+
+        System.out.println("We are deciding on the price for our chocolate cupcake. Here is the description:");
+        chocolate.type();
+        System.out.println("How much would you like to charge for the cupcake?");
+        priceText = input.nextLine();
+        price = Double.parseDouble(priceText);
+        chocolate.setPrice(price);
+        cupcakeMenu.add(cupcake);
+        cupcakeMenu.add(redVelvet);
+        cupcakeMenu.add(chocolate);
+        System.out.println(cupcakeMenu);
+
+
+
+
+    }
+}
